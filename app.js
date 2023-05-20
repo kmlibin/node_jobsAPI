52d46abca0b79a194ebf8a6afc41b9718419aa91
Kelli Libin <km.putnam2@gmail.com>
2023-05-20 11:45:16 -0600

    connect to DB

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
 //set up server
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //connectDB
+const connectDB = require("./db/connect");
 
 //router
-const authRouter = require('./routes/auth');
-const jobsRouter = require('./routes/auth');
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/auth");
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 //invoke for post routes, get req.body
 app.use(express.json());
 // extra packages
 
 // routes
-app.use('/api/vi/auth', authRouter);
-app.use('/api/vi/jobs', jobsRouter);
+app.use("/api/vi/auth", authRouter);
+app.use("/api/vi/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -30,6 +31,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
