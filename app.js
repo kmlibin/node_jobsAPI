e4cf3893a8b9433bebed1ce985a370e573dcefff
Kelli Libin <km.putnam2@gmail.com>
2023-05-20 15:39:59 -0600

    fixed typo

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.use("/api/vi/auth", authRouter);
-app.use("/api/vi/jobs", jobsRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
