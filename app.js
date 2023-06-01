08b9efeafb724cee3378bbaf8ea99d228019deb0
Kelli Libin <km.putnam2@gmail.com>
2023-06-01 14:45:14 -0600

    added security packages

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
 
+//extra security pckges
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+
 //set up server
 const express = require("express");
 const app = express();
@@ -19,10 +25,23 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+//set up rateLimit
+app.set('trust proxy', 1);
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, //15 min
+  max: 100 //each IP has 100 requests per 15 min
+}));
 
 //invoke for post routes, get req.body
 app.use(express.json());
-// extra packages
+
+//security
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+
+
 
 // routes
 app.use("/api/v1/auth", authRouter);
