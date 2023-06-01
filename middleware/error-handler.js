f8e3320093331cad2b5c44b278a07bab830c841c
Kelli Libin <km.putnam2@gmail.com>
2023-06-01 14:04:13 -0600

    validation and cast errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,11 +6,27 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "something went wrong",
   };
+  //handling validation errors (no name, email, pass)
+  //validationerror? hits badrequest, not this error
+  if (err.name === 'ValidatorError') {
+    console.log(Object.values(err.errors));
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',')
+    customError.statusCode = 400
+  }
 
   //duplicate error
   if (err.code && err.code === 11000) {
-    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field`;
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field`;
     customError.statusCode = 400;
+  }
+  //cast errors
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id : ${err.value}`
+    customError.statusCode = 404
   }
   return res.status(customError.statusCode).json({ msg: customError.msg });
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
