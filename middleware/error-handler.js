d5e7d3c8fcb3fbccb5e2ddfae763639ab1f400b1
Kelli Libin <km.putnam2@gmail.com>
2023-06-01 13:43:55 -0600

    remove customAPIerror

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,3 @@
-const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
@@ -7,14 +6,13 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "something went wrong",
   };
-  if (err instanceof CustomAPIError) {
-    return res.status(customError.statusCode).json({ msg: customError.msg });
-  }
+
   //duplicate error
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field`;
     customError.statusCode = 400;
   }
+  return res.status(customError.statusCode).json({ msg: customError.msg });
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
 };
 
